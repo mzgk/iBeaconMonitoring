5594bb68ee4c922123fbc918d3833a2eb22fd815
mzgk <mzgkworks@gmail.com>
2018-03-05 14:31:51 +0900

    Initial Commit

---
+++ iBeaconMonitoring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconMonitoring
+//
+//  Created by mzgk on 2018/03/05.
+//  Copyright © 2018年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
